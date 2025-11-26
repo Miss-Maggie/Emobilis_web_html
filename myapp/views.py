a13cd6f951243326d3b36c4fcfd6b803793097a1
Miss-Maggie <magdalinemutave001@gmail.com>
2025-11-26 09:32:18 +0300

    Added backend logic

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from myapp.models import Contact
+
 
 # Create your views here.
 def index(request):
@@ -11,6 +14,14 @@ def about(request):
     return render(request, '404.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        query = Contact(name=name, email=email, subject=subject, message=message)
+        query.save()
+        return redirect("/")
     return render(request, 'contact.html')
 
 def donation(request):
